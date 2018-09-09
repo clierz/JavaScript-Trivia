fd7cdc5d02dd579e126742ada7e8c35f5a3ed46f
Caren Lierz <carenlierz@hotmail.com>
2018-09-09 18:23:59 -0500

    small changes to html and adding JS file

---
+++ JS/main.js
@@ -0,0 +1,55 @@
+// Quiz questions
+
+const myQuestions = [
+    {
+        quesNum: 1,
+        question: "The purpose of JavaScript on a website is to?",
+        answers: {
+            a: "Define the content of web pages",
+            b: "Program the behavior of web pages",
+            c: "Specify the layout of web pages",
+        },
+    correctAnswer: "b"
+    },
+    {
+        quesNum: 2,
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: {
+            a: "<JavaScript",
+            b: "<header>",
+            c: "<script>",
+        },
+    correctAnswer: "c",
+    },
+    {
+        quesNum: 3,
+        question: "Where is the correct place to insert a JavaScript?",
+        answers: {
+            a: "The <body> section",
+            b: "The <head> section",
+            c: "Both the <head> section and the <body> section are correct",
+        },
+    correctAnswer: "c",
+    },
+    {
+        quesNum: 4,
+        question: "What is the correct syntax for referring to an external script called xxx.js?",
+        answers: {
+            a: "<script href=xxx.js>",
+            b: "<script src=xxx.js",
+            c: "<script name=xxx.js",
+        },
+    correctAnswer: "b",
+    },
+    {
+        quesNum: 5,
+        question: "How do you write 'Hello World' in an alert box?",
+        answers: {
+            a: "msg('Hello World')",
+            b: "alert('Hello World')",
+            c: "alertbox('Hello World')",
+        },
+    correctAnswer: "b",
+    },
+
+]
